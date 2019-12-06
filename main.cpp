bffc03b1ec0d245bdce18338ea7312f9df3c3bcd
Kayn Serhal <Kayn_Serhal@protonmail.com>
2019-12-06 17:04:11 +0100

    added a fun comment

--- main.cpp
+++ main.cpp
@@ -13,6 +13,17 @@
 
 
 
+
+
+
+//MettatonSquared v0.0.1
+//The goal? Try to make a prototype of a brand new fighting system for RPGs!
+//For that, i will first try to remade the undertale combat system using SDL.
+//Then, it will be Deltarune...
+//And from that, i will try to implement mine.
+//Mettaton EX will be the dummy enemy that i will use for this demo.
+//I mean...
+//You can't have too much Mettaton, and this is going to take a while...
 using namespace std;
 
 const int SWIDTH = 640;
